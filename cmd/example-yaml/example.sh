d61424e1e557c322dff47b48390373a8ada95066
Krzysztof Reczek <krzysztof.reczek@karhoo.com>
2021-06-25 12:18:12 +0200

    Fix example tests

--- cmd/example-yaml/example.sh
+++ cmd/example-yaml/example.sh
@@ -7,6 +7,10 @@ rm -rf .out && mkdir .out
 go run main.go
 echo "Example structure scraped successfully and rendered to: .out/output.plantuml"
 
+if [ "$1" = "--test" ]; then
+  exit 0
+fi
+
 if ! which plantuml > /dev/null; then
   echo "Plantuml is not available. In order to generate example PNG, please install it and run the script again."
   exit 1
